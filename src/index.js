1ebeefc324c087346dc25db2eb26879b9ffaa63a
fslm5415 <fslm5415@gmail.com>
2021-03-12 16:19:58 +0900

    Finich the project in video.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import firebase from "firebase/app";
-// import 'firebase/firestore';
+import 'firebase/firestore';
 import { Provider } from "react-redux";
 import store from "./store/index";
 
