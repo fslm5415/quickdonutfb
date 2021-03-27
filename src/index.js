14dd0cb3e517123cb2ef088847a1d01b4023a568
fslm5415 <fslm5415@gmail.com>
2021-03-27 19:47:38 +0900

    Modified some files.

--- src/index.js
+++ src/index.js
@@ -6,19 +6,9 @@ import                      'firebase/firestore';
 import App             from './App';
 import reportWebVitals from './reportWebVitals';
 import store           from "./store/index";
+import firebaseConfig  from "./firebaseConfig";
 import                      './index.css';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey           : "AIzaSyD_BdPqp8QLDE05d4tCrIaeaNeBiH-6pOs",
-  authDomain       : "quickdonutfb.firebaseapp.com",
-  projectId        : "quickdonutfb",
-  storageBucket    : "quickdonutfb.appspot.com",
-  messagingSenderId: "510153669984",
-  appId            : "1:510153669984:web:6868d896e62d34fef07205",
-  measurementId    : "G-2E8CKB9B4Y"
-};
 firebase.initializeApp(firebaseConfig);
 
 window.store = store;
